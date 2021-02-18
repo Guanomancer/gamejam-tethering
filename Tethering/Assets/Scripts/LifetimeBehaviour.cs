ae445ce5ade3fa308cf750f6bfe4018b476ff510
Guanomancer <arch@guanomancer.com>
2021-02-18 14:51:14 +0100

    Added objects for the high score

--- Tethering/Assets/Scripts/LifetimeBehaviour.cs
+++ Tethering/Assets/Scripts/LifetimeBehaviour.cs
@@ -7,6 +7,9 @@ public class LifetimeBehaviour : MonoBehaviour
     [SerializeField]
     private float _lifeTime = 1f;
 
+    [SerializeField]
+    private GameObject SpawnOnEndOfLife;
+
     private void OnEnable()
     {
         Destroy(gameObject, _lifeTime);
